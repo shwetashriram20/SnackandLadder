9df9bff20ad710615470d15b52d37c9a7e4737f2
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-28 14:21:31 +0530

    GameStart

---
+++ SnakeLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackandLadder
+{
+    public class SnakeLadder
+    {
+        public static void Gamestart()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder single player  : " + startPosition);
+        }
+    }
+}
+    
+
