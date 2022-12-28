c796478422237cda6493cf554ae172917ba40703
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-28 14:37:09 +0530

    Dice number

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -11,7 +11,17 @@ namespace SnackandLadder
         public static void Gamestart()
         {
             int startPosition = 0;
+           
+            Random random = new Random();
+
+            int dice = random.Next(1, 6);
+
+
             Console.WriteLine("Snake and Ladder single player  : " + startPosition);
+            
+
+            Console.WriteLine("The Players dice number is  1 to 6 is : " + dice);
+
         }
     }
 }
