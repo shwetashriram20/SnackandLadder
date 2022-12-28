5c96b91d3c69ada46e83a513024c774350a8b0d8
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-28 16:04:05 +0530

    Player Position Moves

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -10,25 +10,92 @@ namespace SnackandLadder
     {
         public static void Gamestart()
         {
+            int position = 0;
+            int currentPosition = 0;
+            int endPosition = 100;
             int startposition = 0;
-            Random random = new Random();
-            int dice = random.Next(1, 6);
-            int move = random.Next(3);
             const int NoPlay = 0;
-            const int snake = 1;
-            const int ladder = 2;
-            switch (move)
+            const int ladder = 1;
+            const int snake = 2;
+            int temp = 0;
+           
+            while (currentPosition < endPosition)
             {
-                case NoPlay:
-                    break;
-                case snake:
-                    startposition = dice;
-                    break;
-                case ladder:
-                    startposition = startposition - dice;
-                    break;
+                Random random = new Random();
+                int die = random.Next(1, 6); 
+                int move = random.Next(3);   
+                switch (move)
+                {
+                    case NoPlay:
+                        position = 0;
+                        break;
+                    case ladder:
+                        position = die;
+                        break;
+                    case snake:
+                        if (currentPosition > 0)
+                        {
+                            position = 0;
+                            currentPosition = currentPosition - die;
+                            if (currentPosition < 0)
+                            {
+                                currentPosition = startposition;
+                            }
+                        }
+                        else
+                        {
+                            currentPosition = startposition;
+                        }
+                        break;
+                }
+
+                currentPosition += (position);
+                if (currentPosition <= 100)
+                {
+                    Console.WriteLine("die::{0}option::{1} position::{2}", die, move, currentPosition);
+                    temp = currentPosition;
+                }
+
+
+                if (endPosition < currentPosition)
+                {
+                    while (temp < endPosition)
+                    {
+                        Random random1 = new Random();
+                        int die1 = random.Next(1, 6); 
+                        int move1 = random.Next(3);   
+                        switch (move1)
+                        {
+                            case NoPlay:
+                                position = 0;
+                                break;
+                            case ladder:
+                                if (temp + die1 == endPosition)
+                                {
+                                    position = die1;
+                                }
+                                else
+                                {
+                                    position = 0;
+                                }
+                                break;
+                            case snake:
+                                position = 0;
+                                break;
+                        }
+                        temp += (position);
+                        if (temp == endPosition)
+                        {
+                            Console.WriteLine("die::{0} option::{1} position::{2}", die1, move1, temp);
+                        }
+                    }
+
+                }
             }
-            Console.WriteLine("dice::{0},option::{1},position::{2}", dice, move, startposition);
         }
+
+
     }
 }
+
+                       
